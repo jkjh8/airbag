21801f13c8086aefcca82ec32aadb339b752f226
jkjh8 <jkjh82@naver.com>
2022-12-10 02:58:16 +0900

    first commit

---
+++ src-electron/electron-preload.js
@@ -0,0 +1,12 @@
+import { contextBridge, ipcRenderer } from "electron";
+
+contextBridge.exposeInMainWorld("FN", {
+  onRequest: (args) => {
+    ipcRenderer.send("onRequest", { ...args });
+  },
+  onResponse: (fn) => {
+    ipcRenderer.on("onResponse", (event, ...args) => {
+      fn(...args);
+    });
+  },
+});
